ad6cbcfbb5935c3e1ec0dd36874bab0e7bc9ed5a
Valerai Dementyeva <lera.dementjeva@yandex.ru>
2023-07-26 17:33:50 +0300

    All functionality of the game v2
    v2. We get the value from the user, convert it to a number, compare it with the guessed number from the computer. We output the result to the console.

--- UnknownNumberMacOS/main.swift
+++ UnknownNumberMacOS/main.swift
@@ -5,45 +5,66 @@
 //  Created by Валерия Дементьева on 26.07.2023.
 //
 
-import Foundation
-// генерация случайного положительного числа
-let randomNumber = UInt8.random(in: 1...250)
-// в переменную будет записываться число с консоли
-var myNumber: String?
-//преобразовывает полученную строку в число
-func transformationStrInUint(_ str: String?) -> UInt8{
-    var result = UInt8()
-    if let str = str {
-        if UInt8(str) != nil {
-            result = UInt8(str)!
-        } else {
-            return 0
-        }
-    } else {
-        return 0
-    }
-    return result
-}
+//import Foundation
+//// генерация случайного положительного числа
+//let randomNumber = UInt8.random(in: 1...250)
+//// в переменную будет записываться число с консоли
+//var myNumber: String?
+////преобразовывает полученную строку в число
+//func transformationStrInUint(_ str: String?) -> UInt8{
+//    var result = UInt8()
+//    if let str = str {
+//        if UInt8(str) != nil {
+//            result = UInt8(str)!
+//        } else {
+//            return 0
+//        }
+//    } else {
+//        return 0
+//    }
+//    return result
+//}
+//print("Компьютер случайным образом загадал число от 1 до 250. Вам требуется отгадать его.")
+//// цикл с постпроверкой условия
+//repeat {
+//    print("Введите ваш вариант и нажмите Enter!")
+//    // получение значения с клавиатуры пользователя
+//    myNumber = readLine()
+//    //преобразование полученной строки в число
+//    let transformationNamder = transformationStrInUint(myNumber)
+//    // сравнение введенного и сгенерированного чисел
+//    if transformationNamder == 0 {
+//        print("Значение введено не корректно, начните игру сначала!")
+//        break
+//    } else if transformationNamder == randomNumber{
+//        print("Вы угадали!")
+//    } else if transformationNamder < randomNumber{
+//        print("Ваше число меньше загаданного, попробуйте еще раз.")
+//    } else if transformationNamder > randomNumber{
+//        print("Ваше число больше загаданного, попробуйте еще раз.")
+//    }
+//} while randomNumber != transformationStrInUint(myNumber)
+
+
+
 print("Компьютер случайным образом загадал число от 1 до 250. Вам требуется отгадать его.")
-// цикл с постпроверкой условия
+// Случайное число
+let randomNumber = UInt8.random(in: 1...250)
+print("Введите ваш вариант и нажмите Enter!")
+// флаг-указатель на победу
+var isWin = false
 repeat {
-    print("Введите ваш вариант и нажмите Enter!")
-    // получение значения с клавиатуры пользователя
-    myNumber = readLine()
-    //преобразование полученной строки в число
-    let transformationNamder = transformationStrInUint(myNumber)
-    // сравнение введенного и сгенерированного чисел
-    if transformationNamder == 0 {
+    // попытка преобразования введенного значения к UInt8
+    guard let userNumber = UInt8(readLine() ?? "") else {
         print("Значение введено не корректно, начните игру сначала!")
-        break
-    } else if transformationNamder == randomNumber{
-        print("Вы угадали!")
-    } else if transformationNamder < randomNumber{
+        continue }
+    // проверка введенного числа
+    if userNumber < randomNumber{
         print("Ваше число меньше загаданного, попробуйте еще раз.")
-    } else if transformationNamder > randomNumber{
+    } else if userNumber > randomNumber {
         print("Ваше число больше загаданного, попробуйте еще раз.")
+    } else {
+        print("Вы угадали")
+        isWin = true
     }
-} while randomNumber != UInt8(myNumber!)
-
-
-
+} while !isWin
