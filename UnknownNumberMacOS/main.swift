cecf6c05838705a0f3e2a2a57c8d950560dfa701
Valerai Dementyeva <lera.dementjeva@yandex.ru>
2023-07-26 17:18:21 +0300

    All functionality of the game
    We get the value from the user, convert it to a number, compare it with the guessed number from the computer. We output the result to the console.

--- UnknownNumberMacOS/main.swift
+++ UnknownNumberMacOS/main.swift
@@ -6,6 +6,44 @@
 //
 
 import Foundation
+// генерация случайного положительного числа
+let randomNumber = UInt8.random(in: 1...250)
+// в переменную будет записываться число с консоли
+var myNumber: String?
+//преобразовывает полученную строку в число
+func transformationStrInUint(_ str: String?) -> UInt8{
+    var result = UInt8()
+    if let str = str {
+        if UInt8(str) != nil {
+            result = UInt8(str)!
+        } else {
+            return 0
+        }
+    } else {
+        return 0
+    }
+    return result
+}
+print("Компьютер случайным образом загадал число от 1 до 250. Вам требуется отгадать его.")
+// цикл с постпроверкой условия
+repeat {
+    print("Введите ваш вариант и нажмите Enter!")
+    // получение значения с клавиатуры пользователя
+    myNumber = readLine()
+    //преобразование полученной строки в число
+    let transformationNamder = transformationStrInUint(myNumber)
+    // сравнение введенного и сгенерированного чисел
+    if transformationNamder == 0 {
+        print("Значение введено не корректно, начните игру сначала!")
+        break
+    } else if transformationNamder == randomNumber{
+        print("Вы угадали!")
+    } else if transformationNamder < randomNumber{
+        print("Ваше число меньше загаданного, попробуйте еще раз.")
+    } else if transformationNamder > randomNumber{
+        print("Ваше число больше загаданного, попробуйте еще раз.")
+    }
+} while randomNumber != UInt8(myNumber!)
 
-print("Hello, World!")
+
 
